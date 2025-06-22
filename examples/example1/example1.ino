d62e8905bc895d225d0417a8915537611bde06e4
Pygramer78 <ayuskyap@gmail.com>
2025-06-22 22:11:32 +0200

    Example updated

--- examples/example1/example1.ino
+++ examples/example1/example1.ino
@@ -1,9 +1,17 @@
 #include <smart_irrigation_device.h>
 
+const int threshold = 35;
+const int sensor_pin = A0;
+const int relayPin = 8;
+
+
 void setup() {
-  activatePinMode();
+  // Serial.begin(9600); <-- If needed
+  activatePinMode(relayPin, sensor_pin); // Activates both pins
+  // Serial.println("Pin activated") <-- Only if needed
 }
 
 void loop() {
-  readMoisture();
+  int moisture = readMoisture(); // reads Moisture and saves it in variable
+  controlRelay(relayPin, moisture, threshold); // The relay pin, your moisture variable and the threshold (when the water starts pumping)
 }
