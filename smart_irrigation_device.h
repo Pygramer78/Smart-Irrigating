9a50a0af0aed5cfb9667ba51e1675b6fabb34310
Pygramer78 <ayuskyap@gmail.com>
2025-06-26 14:23:26 +0200

    Not necesary stuff eliminated

--- smart_irrigation_device.h
+++ smart_irrigation_device.h
@@ -1,16 +1,6 @@
 /* MADE BY Pygramer78 & Pablogpus*/
 /*__________/ INIT COMMENTS \_____________*/
 /*
-Things to install;
- AHT20: https://github.com/dvarrel/AHT20
-  TSL2591: https://github.com/adafruit/Adafruit_TSL2591_Library
-  BMP280: https://github.com/adafruit/Adafruit_BMP280_Library
-  MLX90615: https://github.com/skiselev/MLX90615
-
-  My library;
-  https://github.com/Pygramer78/Arduino-Libs
-
-
 LICENSE:
 GPL-3 (GNU public license)
 */
